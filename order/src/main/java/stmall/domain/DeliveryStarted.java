2e65157ffd1c4743f40c524ce777f5a2d9f16d5f
박용주 <apexacme@gmail.com>
2025-04-03 20:35:16 +0900

    Update DeliveryStarted.java

--- order/src/main/java/stmall/domain/DeliveryStarted.java
+++ order/src/main/java/stmall/domain/DeliveryStarted.java
@@ -8,7 +8,7 @@ import stmall.infra.AbstractEvent;
 @Data
 @ToString
 public class DeliveryStarted extends AbstractEvent {
-
+ 
     private Long id;
     private Long orderid;
     private String productname;
